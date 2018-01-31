256dc212a1a3bdef336c0506497b6dc63bbf2b48
shott-mie <shott@mieweb.com>
2018-01-31 14:29:26 -0500

    Updating app to open App Test WebChart system

--- platforms/windows/www/js/index.js
+++ platforms/windows/www/js/index.js
@@ -29,20 +29,19 @@ var app = {
         app.receivedEvent('deviceready');
 
         // Here, we redirect to the web site.
-        var targetUrl = "https://zeus-web.med-web.com/webchart/wctshott/webchart.cgi?";
-        var sysLink = document.getElementById("sysLink");
-        sysLink.setAttribute("href", targetUrl);
-        sysLink.text = targetUrl;
+        var targetUrl = "https://webchartnow.com/apptest/";
         window.location.replace(targetUrl);
 	},
     // Note: This code is taken from the Cordova CLI template.
     receivedEvent: function(id) {
+		/*
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
+		*/
 
         console.log('Received Event: ' + id);
     }
